08ffcd9f55b29dfd6b90d6ccd4bf3ab0e8a5118c
Oodi <simo.auvinen@helsinki.fi>
2016-04-11 16:39:04 +0300

    Projekti pohjaa

---
+++ src/main/java/com/pohjantahti/ohtumini/App.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pohjantahti.ohtumini;
+
+
+public class App {
+        public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
