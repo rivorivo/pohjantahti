b612095d6cfb896ecd991476098f97df204baf4e
Hanski Joni P <jphanski@dat-10.cs.helsinki.fi>
2016-04-11 16:55:58 +0300

    rename

--- src/main/java/ohtumini/App.java
+++ src/main/java/ohtumini/App.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pohjantahti.ohtumini;
+package ohtumini;
 
 import java.util.Locale;
 import java.util.Scanner;
