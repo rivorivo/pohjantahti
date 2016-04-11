1a650a0cd6c5276c47b6118bb622a0ee2e3ad2e3
rivorivo <rikuvo@gmail.com>
2016-04-11 19:43:51 +0300

    IOluokka

---
+++ src/main/java/io/IO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package io;
+
+/**
+ *
+ * @author rivorivo
+ */
+public interface IO {
+    void Print(String toPrint);
+    int readInt(String prompt);
+    String readLine(String prompt);
+}
---
+++ src/main/java/io/StubIO.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package io;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rivorivo
+ */
+public class StubIO implements IO {
+    
+    private String[] lines;
+    private int i;
+    private ArrayList<String> prints;
+    
+    public StubIO(String... values) {
+        this.lines=values;
+        prints=new ArrayList<String>();
+    }
+       public void print(String toPrint) {
+        prints.add(toPrint);
+    }
+
+    public int readInt(String prompt) {
+        print(prompt);
+        return Integer.parseInt(lines[i++]);
+    }
+
+    public ArrayList<String> getPrints() {
+        return prints;
+    }
+
+    public String readLine(String prompt) {
+        print(prompt);
+        if (i < lines.length) {
+            return lines[i++];
+        }
+        return "";
+    } 
+
+    @Override
+    public void Print(String toPrint) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
