ae35a6f6bf7d269ff0085397cff0095c2e29675b
Hanski Joni P <jphanski@dat-10.cs.helsinki.fi>
2016-04-11 16:42:55 +0300

    Komentolooppi

--- src/main/java/com/pohjantahti/ohtumini/App.java
+++ src/main/java/com/pohjantahti/ohtumini/App.java
@@ -5,9 +5,25 @@
  */
 package com.pohjantahti.ohtumini;
 
+import java.util.Locale;
+import java.util.Scanner;
 
 public class App {
-        public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String komento;
+        while (true) {
+            komento = reader.nextLine();
+            komento = komento.toUpperCase(Locale.ROOT);
+            if (komento.startsWith("FIRST COMMAND")) {
+                //Juttuja
+
+            } else if (komento.startsWith("SECOND_COMMAND")) {
+                //Muita juttuja
+            } else {
+                System.out.println("? ");
+            }
+        }
     }
 }
