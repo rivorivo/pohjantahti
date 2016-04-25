815488ec2a3918d83457a954685b715a930d5adb
Vornanen Riku H <rivorivo@dat-5.cs.helsinki.fi>
2016-04-25 15:11:21 +0300

    viitelista yksiktest

--- src/test/java/viitteet/ViitelistaTest.java
+++ src/test/java/viitteet/ViitelistaTest.java
@@ -10,8 +10,77 @@ import static org.junit.Assert.*;
 
 public class ViitelistaTest {
     
+    private Viitelista lista;
+    private String nimi = "lide";
     public ViitelistaTest() {
+        lista = new Viitelista(nimi);        
     }
+    
+    public void alustaViite(){
+        
+    }
+    
+    
+    @Test
+    public void lisaaminenToimii(){
+        Article a = new Article(); 
+        a.tunniste="t";      
+        lista.add(a);          
+        assertFalse(lista.getViitteet().isEmpty());        
+    }
+    
+    @Test
+    public void listastaPoistaminenToimii(){
+         Article a = new Article(); 
+        a.tunniste="t";      
+        lista.add(a); 
+        assertFalse(lista.getViitteet().isEmpty()); 
+        lista.remove("t");
+        assertTrue(lista.getViitteet().isEmpty());
+    }
+    
+    @Test
+    public void getViiteToimii(){
+        Article a = new Article(); 
+        a.tunniste="t"; 
+        lista.add(a);
+        assertEquals(lista.get("t"),a);
+    }
+    
+    @Test
+    public void getViitteetToimii(){
+        Article a = new Article(); 
+        a.tunniste="t"; 
+        Book b = new Book();
+        b.tunniste="t2";
+        ArrayList<Viite> viitteet = new ArrayList<>();
+        viitteet.add(a);
+        viitteet.add(b);
+        lista.add(a);
+        lista.add(b);
+                assertEquals(lista.getViitteet(),viitteet);    
+    }
+    
+    @Test
+    public void getNimiToimii(){
+        assertEquals(lista.getNimi(),"lide");
+    }
+    
+     @Test
+    public void sizeToimii(){
+         Article a = new Article(); 
+        a.tunniste="t"; 
+        Book b = new Book();
+        b.tunniste="t2";
+        Inproceedings i = new Inproceedings();
+        i.tunniste="t3";
+        lista.add(i);
+        lista.add(a);
+        lista.add(b);
+        assertEquals(lista.size(),3);
+    }
+    
+    
     
     @BeforeClass
     public static void setUpClass() {
